387c44a0f25f537b97c854eebf6f3d5371145510
Jerry <panwork1990@qq.com>
2018-06-12 23:45:47 +0800

    Siamese损失函数和激活函数

---
+++ src/main/java/personal/pan/dl4j/nn/activations/ActivationSiamese.java
@@ -0,0 +1,77 @@
+package personal.pan.dl4j.nn.activations;
+
+import org.nd4j.linalg.activations.BaseActivationFunction;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.BooleanIndexing;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.linalg.indexing.conditions.Conditions;
+import org.nd4j.linalg.primitives.Pair;
+
+public class ActivationSiamese extends BaseActivationFunction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final double threshold;
+
+	public ActivationSiamese(double threshold) {
+		this.threshold = threshold;
+	}
+
+	@Override
+	public INDArray getActivation(INDArray in, boolean training) {
+		return siamese(in);
+	}
+
+	/*
+	 * dLda=epsilon,在Siamese损失函数的反向传播时，此处的epsilon=labels
+	 */
+	@Override
+	public Pair<INDArray, INDArray> backprop(INDArray in, INDArray epsilon) {
+		int row = epsilon.rows();
+		INDArray dLdz = Nd4j.create(row, 1);
+
+		INDArray derivativeNegative = in.dup(in.ordering());
+
+		BooleanIndexing.replaceWhere(derivativeNegative, 0, Conditions.greaterThanOrEqual(threshold));
+		BooleanIndexing.replaceWhere(derivativeNegative, in.mul(2), Conditions.notEquals(0));
+
+		INDArray derivativePositive = in.rsub(1).div(2);
+
+		INDArray derivative = Nd4j.create(in.shape());
+
+		derivative.put(new INDArrayIndex[] { NDArrayIndex.all(), NDArrayIndex.point(0) }, derivativeNegative);
+		derivative.put(new INDArrayIndex[] { NDArrayIndex.all(), NDArrayIndex.point(1) }, derivativePositive);
+
+		for (int i = 0; i < row; i++) {
+			INDArray v1 = derivative.get(new INDArrayIndex[] { NDArrayIndex.point(i), NDArrayIndex.all() });
+			INDArray v2 = epsilon.get(new INDArrayIndex[] { NDArrayIndex.point(i), NDArrayIndex.all() });
+
+			dLdz.put(new INDArrayIndex[] { NDArrayIndex.point(i), NDArrayIndex.all() }, v1.mmul(v2.transpose()));
+		}
+
+		return new Pair<>(dLdz, null);
+	}
+
+	protected INDArray siamese(INDArray in) {
+		INDArray outputPositive = in.rsub(1).div(2);
+		outputPositive.muli(outputPositive);
+
+		INDArray outputNegative = in.dup(in.ordering());
+
+		BooleanIndexing.replaceWhere(outputNegative, 0, Conditions.greaterThanOrEqual(threshold));
+		BooleanIndexing.replaceWhere(outputNegative, in.mul(in), Conditions.notEquals(0));
+
+		INDArray output = Nd4j.create(in.shape());
+
+		output.put(new INDArrayIndex[] { NDArrayIndex.all(), NDArrayIndex.point(0) }, outputNegative);
+		output.put(new INDArrayIndex[] { NDArrayIndex.all(), NDArrayIndex.point(1) }, outputPositive);
+
+		return output;
+	}
+
+}
--- src/main/java/personal/pan/dl4j/nn/lossfunctions/LossSiamese.java
+++ src/main/java/personal/pan/dl4j/nn/lossfunctions/LossSiamese.java
@@ -2,8 +2,17 @@ package personal.pan.dl4j.nn.lossfunctions;
 
 import org.nd4j.linalg.activations.IActivation;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.lossfunctions.LossUtil;
 import org.nd4j.linalg.lossfunctions.impl.LossMCXENT;
 
+import personal.pan.dl4j.nn.activations.ActivationSiamese;
+
+/**
+ * https://blog.csdn.net/thriving_fcl/article/details/73730552
+ * 
+ * @author Jerry
+ *
+ */
 public class LossSiamese extends LossMCXENT {
 
 	/**
@@ -11,9 +20,79 @@ public class LossSiamese extends LossMCXENT {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * for (int i = 0; i < row; i++) {<br>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;double ew = output.getDouble(i, 0);<br>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;yhatNegative.putScalar(i, 0, ew < threshold ? ew * ew
+	 * : 0);<br>
+	 * }<br>
+	 * 
+	 * @param labels
+	 *            shape(batchSize,2)
+	 * @param preOutput
+	 *            shape(batchSize,1)&nbsp;&nbsp;&nbsp;&nbsp;此参数为链接中的Ew,即cosine值
+	 * @param activationFn
+	 *            {@link ActivationSiamese}
+	 * @param mask
+	 * @return
+	 */
+	private INDArray scoreArray(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask) {
+		if (labels.size(1) != (preOutput.size(1) + 1)) {
+			throw new IllegalArgumentException("Labels array numColumns (size(1) = " + String.valueOf(labels.size(1))
+					+ ") does not match output layer" + " number of outputs (nOut = "
+					+ String.valueOf(preOutput.size(1) + 1) + ") ");
+
+		}
+
+		if (!(activationFn instanceof ActivationSiamese)) {
+			throw new RuntimeException("activation function must be ActivationSiamese.");
+		}
+
+		INDArray output = activationFn.getActivation(preOutput.dup(), true);
+
+		INDArray scoreArr = output.mul(labels);
+
+		if (mask != null) {
+			LossUtil.applyMask(scoreArr, mask);
+		}
+
+		return scoreArr;
+	}
+
+	@Override
+	public double computeScore(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask,
+			boolean average) {
+		INDArray scoreArr = scoreArray(labels, preOutput, activationFn, mask);
+
+		double score = scoreArr.sumNumber().doubleValue();
+
+		if (average) {
+			score /= scoreArr.size(0);
+		}
+
+		return score;
+	}
+
+	@Override
+	public INDArray computeScoreArray(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask) {
+		INDArray scoreArr = scoreArray(labels, preOutput, activationFn, mask);
+		return scoreArr.sum(1);
+	}
+
+	/*
+	 * @param preOutput
+	 *            shape(batchSize,1)&nbsp;&nbsp;&nbsp;&nbsp;此参数为链接中的Ew,即cosine值
+	 */
 	@Override
 	public INDArray computeGradient(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask) {
-		// do it in future
-		return super.computeGradient(labels, preOutput, activationFn, mask);
+
+		if (!(activationFn instanceof ActivationSiamese)) {
+			throw new RuntimeException("activation function must be ActivationSiamese.");
+		}
+
+		INDArray dLda = labels.dup();
+		INDArray gradients = activationFn.backprop(preOutput, dLda).getFirst();
+
+		return gradients;
 	}
 }
